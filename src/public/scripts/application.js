8da994670fe07b28c4b1c0511d4d370b036cecb1
Nick <nsteffens@gmail.com>
2012-03-14 22:14:30 -0500

    added basic lightbox functionality

--- src/public/scripts/application.js
+++ src/public/scripts/application.js
@@ -4,11 +4,48 @@ Application controlling all functions
 
 */
 NS = {
-	
+	// collection of all jquery objects
+	objects: {
+		thumb: $('.thumb'),
+		overlay: $('.overlay'),
+		lightbox: $('.lightbox')
+	},
 	// init all functions
 	init: function() {
-		
-		console.log('init');
-		
+		EventListener.thumbClicked();
 	}
+}
+
+EventListener = {
+	// listens for art thumbs to be clicked
+	thumbClicked: function() {
+		NS.objects.thumb.click(function(e){
+			e.preventDefault();
+			var self = $(this).attr('title');
+			Render.toggleOverlay();
+			Render.toggleLightBox();
+			EventListener.closeOverlay();
+		});
+	},
+	
+	// listens for overlay or close to be clicked to clicked
+	closeOverlay: function() {
+		NS.objects.overlay.click(function(){
+			Render.toggleOverlay();
+			Render.toggleLightBox();
+		});
+	}
+	
+}
+
+Render = {
+	
+	toggleOverlay: function() {
+		NS.objects.overlay.toggle();
+	},
+	
+	toggleLightBox: function() {
+		NS.objects.lightbox.toggle();
+	}
+	
 }
