5ad3bfa955a914e337b8206e261c156ad4bca08e
Nick Steffens <nick.steffens@designkitchen.com>
2012-05-14 22:04:52 -0500

    init

--- out/scripts/rollover.js
+++ out/scripts/rollover.js
@@ -1,95 +1,95 @@
 
 // Gallery
-		function galleryrollover(){
+        function galleryrollover(){
 
-			document.getElementById("gallery").src = "images/gallery_RO.png";
-		
-		}
+            document.getElementById("gallery").src = "images/gallery_RO.png";
+        
+        }
 
-		function galleryrolloff(){		
-		
-			document.getElementById("gallery").src = "images/gallery.png";
-		
-		}
-		
-// About Me		
-		function productionrollover(){
+        function galleryrolloff(){        
+        
+            document.getElementById("gallery").src = "images/gallery.png";
+        
+        }
+        
+// About Me        
+        function productionrollover(){
 
-			document.getElementById("production").src = "images/production_RO.png";
-		
-		}
+            document.getElementById("production").src = "images/production_RO.png";
+        
+        }
 
-		function productionrolloff(){		
-		
-			document.getElementById("production").src = "images/production.png";
-		
-		}
+        function productionrolloff(){        
+        
+            document.getElementById("production").src = "images/production.png";
+        
+        }
 
 // FLASH
-		function flashrollover(){
+        function flashrollover(){
 
-			document.getElementById("flash").src = "images/flash_RO.png";
-		
-		}
+            document.getElementById("flash").src = "images/flash_RO.png";
+        
+        }
 
-		function flashrolloff(){		
-		
-			document.getElementById("flash").src = "images/flash.png";
-		
-		}
+        function flashrolloff(){        
+        
+            document.getElementById("flash").src = "images/flash.png";
+        
+        }
 // VIDEO
-		function videorollover(){
+        function videorollover(){
 
-			document.getElementById("video").src = "images/video_RO.png";
-		
-		}
+            document.getElementById("video").src = "images/video_RO.png";
+        
+        }
 
-		function videorolloff(){		
-		
-			document.getElementById("video").src = "images/video.png";
-		
-		}
+        function videorolloff(){        
+        
+            document.getElementById("video").src = "images/video.png";
+        
+        }
 // WEB
-		function webrollover(){
+        function webrollover(){
 
-			document.getElementById("web").src = "images/web_RO.png";
-		
-		}
+            document.getElementById("web").src = "images/web_RO.png";
+        
+        }
 
-		function webrolloff(){		
-		
-			document.getElementById("web").src = "images/web.png";
-		
-		}
+        function webrolloff(){        
+        
+            document.getElementById("web").src = "images/web.png";
+        
+        }
 
 // Email
-	function emailOver()
-	{
-		document.getElementById("email").src ="images/email_RO.png";
-	}
-	function emailOff()
-	{
-		document.getElementById("email").src ="images/email.png";
-	}
+    function emailOver()
+    {
+        document.getElementById("email").src ="images/email_RO.png";
+    }
+    function emailOff()
+    {
+        document.getElementById("email").src ="images/email.png";
+    }
 
-		
+        
 // Name
-		function nsrollover()
-		{
-			document.getElementById("ns").src = "images/nicklassteffens_RO.png";
-		}
-		function nsrolloff()
-		{
-			document.getElementById("ns").src = "images/nicklassteffens.png";
-		}
-			
-// 	--- EXAMPLE ---
+        function nsrollover()
+        {
+            document.getElementById("ns").src = "images/nicklassteffens_RO.png";
+        }
+        function nsrolloff()
+        {
+            document.getElementById("ns").src = "images/nicklassteffens.png";
+        }
+            
+//     --- EXAMPLE ---
 
-		function mouseOver()
-		{
-			document.getElementById("b1").src ="b_blue.gif";
-		}
-		function mouseOut()
-		{
-			document.getElementById("b1").src ="b_pink.gif";
-		}
+        function mouseOver()
+        {
+            document.getElementById("b1").src ="b_blue.gif";
+        }
+        function mouseOut()
+        {
+            document.getElementById("b1").src ="b_pink.gif";
+        }
